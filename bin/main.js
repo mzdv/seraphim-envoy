2e3ea0e1dfaaca30af3e2cd24308955ce70b6f80
Milos Zivadinovic <mzdv@outlook.com>
2015-04-10 21:20:51 +0200

    Bugfix.

--- bin/main.js
+++ bin/main.js
@@ -1,9 +1,7 @@
 var React = require('react');
 
-var ReactApp = React.createFactory(require('').app);
+var ReactApp = React.createFactory(require('./components/ReactApp.jsx'));
 
 var bootstrap = document.getElementById('react-bootstrap');
 
-react.renderComponent(new ReactApp({
-
-}), bootstrap);
+react.renderComponent(new ReactApp({}), bootstrap);
