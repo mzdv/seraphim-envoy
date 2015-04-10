c02d32c901a3614ec79e24f92904813e6e72dd89
Milos Zivadinovic <mzdv@outlook.com>
2015-04-10 21:02:59 +0200

    ReactCounter implementation. I have a lot to read and refactor, this is just an idea mockup.

--- bin/components/ReactApp.jsx
+++ bin/components/ReactApp.jsx
@@ -2,7 +2,7 @@ var React = require('react');
 
 var ReactCounter = React.createFactory(require('./ReactCounter.jsx'));
 var ReactHeader = React.createFactory(require('./ReactHeader.jsx'));
-var ReactTimer = React.createFactory(require('./ReactTimer.jsx'));
+var ReactTime = React.createFactory(require('./ReactTime.jsx'));
 
 var ReactApp = React.createClass({
     // TO-DO: Mounting of components
--- bin/components/ReactCounter.jsx
+++ bin/components/ReactCounter.jsx
@@ -1,7 +1,39 @@
-var react = require('react');
+var React = require('react');
 
-var ReactCounter = react.createClass({
-    // TO-DO: Logic for the counters
+var ReactTitle = React.createClass({
+   render: function() {
+       return(
+         React.createElement('div', {className: 'title'},'')
+       );
+   }
+});
+
+var ReactCounter = React.createClass({
+    render: function() {
+        return(
+            React.createElement('div', {className: 'counter'},'')
+        );
+    }
+});
+
+var ReactTimestamp = React.createClass({
+    render: function() {
+        return(
+            React.createElement('div', {className: 'title'},'')
+        );
+    }
+});
+
+var ReactCounter = React.createClass({
+    render: function() {
+        return(
+            <div class="counterBlock">
+                <ReactTitle />
+                <ReactCounter />
+                <ReactTimestamp />
+            </div>
+        )
+    }
 });
 
 module.exports = ReactCounter;
