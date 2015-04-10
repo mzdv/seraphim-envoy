8407ff2d7e9fc1cf0408565132327ae90d49d172
Milos Zivadinovic <mzdv@outlook.com>
2015-04-10 19:33:53 +0200

    Improved gulpfile to handle changes on the front-end.

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,37 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var livereload = require('gulp-livereload');
+var watch = require('gulp-watch');
+var path = require('path');
 
 //React stuff
 var watchify = require('watchify');
 var browserify = require('browserify');
 var reactify = require('reactify');
 
-gulp.task('develop', function () {
+gulp.task('watch', function() {
+    gulp.src(__dirname)
+        .pipe(watch(path.join(__dirname, '/public/css')))
+        .pipe(watch(path.join(__dirname, '/public/js')))
+        .pipe(watch(path.join(__dirname, '/views')))
+        .pipe(livereload());
+});
+
+gulp.task('restart', function () {
   livereload.listen();
   nodemon({
     script: 'bin/www',
     ext: 'js jade'
   }).on('restart', function () {
     setTimeout(function () {
-      livereload.changed('/views');
+      livereload.changed(__dirname);
     }, 500);
   });
 });
 
+
+
 gulp.task('default', [
-  'develop'
+    'watch',
+    'restart'
 ]);
